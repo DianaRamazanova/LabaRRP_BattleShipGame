79f510b71cd746dd14bcffd5633a8a1447109e09
Диана Рамазанова <diana.nfx11@gmail.com>
2024-12-04 09:05:22 +0300

    Логика игры (1 часть)

--- LabaRRP_BattleShip/Form1.cs
+++ LabaRRP_BattleShip/Form1.cs
@@ -17,6 +17,6 @@ namespace LabaRRP_BattleShip
             InitializeComponent();
         }
 
-        public void 
+       
     }
 }
---
+++ LabaRRP_BattleShip/Game.cs
@@ -0,0 +1,28 @@
+﻿using LabaRRP_BattleShip;
+using System;
+
+namespace BattleShipGame.Core
+{
+    public class Game
+    {
+        public Player Player1 { get; set; }
+        public Player Player2 { get; set; }
+        public bool IsPlayer1Turn { get; set; }
+
+        public Game()
+        {
+            Player1 = new Player();
+            Player2 = new Player();
+            IsPlayer1Turn = true;
+        }
+
+        public void MakeMove(int x, int y)
+        {
+        }
+
+        //public bool IsGameOver()
+       // {
+         //   return 
+        //}
+    }
+}
