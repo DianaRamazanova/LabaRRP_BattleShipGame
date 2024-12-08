c8ea3c821b959d26f5bccdb5722ed6cc5d478839
Диана Рамазанова <diana.nfx11@gmail.com>
2024-12-08 17:15:06 +0300

    Начало работы с интерфейсом

--- LabaRRP_BattleShip/Form1.cs
+++ LabaRRP_BattleShip/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BattleShipGame.Core;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,11 +13,16 @@ namespace LabaRRP_BattleShip
 {
     public partial class Form1 : Form
     {
+        private Game _game;
         public Form1()
         {
             InitializeComponent();
         }
-
-       
+        private void btnStartGame_Click(object sender, EventArgs e)
+        {
+            _game = new Game();
+            //SetupForm setupForm = new SetupForm(_game);
+            // setupForm.ShowDialog();
+        }
     }
 }
