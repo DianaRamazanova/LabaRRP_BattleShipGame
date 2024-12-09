f555e1783e295e869f0ce8c25b1c4c937f371cde
Диана Рамазанова <diana.nfx11@gmail.com>
2024-12-10 00:34:18 +0300

    лидерборд форм и геймовер формс

---
+++ LabaRRP_BattleShip/GameOverForm.cs
@@ -0,0 +1,38 @@
+﻿using BattleShipGame;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabaRRP_BattleShip
+{
+    public partial class GameOverForm : Form
+    {
+        private Player _player;
+
+        public GameOverForm(string result, Player player)
+        {
+            InitializeComponent();
+            _player = player;
+
+        }
+        private void BtnNewGame_Click(object sender, EventArgs e)
+        {
+            Leaderboard leaderboard = new Leaderboard();
+            leaderboard.AddPlayer(_player);
+
+            this.Close();
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+
+}
---
+++ LabaRRP_BattleShip/LeaderBoardForm.cs
@@ -0,0 +1,34 @@
+﻿using BattleShipGame;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabaRRP_BattleShip
+{
+    public partial class LeaderBoardForm : Form
+    {
+        public LeaderBoardForm()
+        {
+            InitializeComponent();
+            LoadLeaderboard();
+        }
+
+        private void LoadLeaderboard()
+        {
+            // Load leaderboard from file
+            Leaderboard leaderboard = new Leaderboard();
+            var topPlayers = leaderboard.GetTopPlayers();
+            lstLeaderboard.Items.Clear();
+            foreach (var player in topPlayers)
+            {
+                lstLeaderboard.Items.Add($"{player.Name} - {player.Score}");
+            }
+        }
+    }
+}
