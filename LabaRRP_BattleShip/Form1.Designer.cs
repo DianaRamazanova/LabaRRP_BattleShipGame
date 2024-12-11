50682ed85e2581b51e9645185341102a43ea2eb7
poniny <blindaglinda@gmail.com>
2024-12-11 21:15:33 +0300

    форм1 дизайнер

--- LabaRRP_BattleShip/Form1.Designer.cs
+++ LabaRRP_BattleShip/Form1.Designer.cs
@@ -28,13 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnStartGame = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtIPAddress = new System.Windows.Forms.TextBox();
+            this.btnLeaderboard = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStartGame
+            // 
+            this.btnStartGame.Location = new System.Drawing.Point(12, 12);
+            this.btnStartGame.Name = "btnStartGame";
+            this.btnStartGame.Size = new System.Drawing.Size(100, 23);
+            this.btnStartGame.TabIndex = 0;
+            this.btnStartGame.Text = "Start Game";
+            this.btnStartGame.UseVisualStyleBackColor = true;
+            this.btnStartGame.Click += new System.EventHandler(this.btnStartGame_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(12, 41);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(100, 23);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtIPAddress
+            // 
+            this.txtIPAddress.Location = new System.Drawing.Point(118, 43);
+            this.txtIPAddress.Name = "txtIPAddress";
+            this.txtIPAddress.Size = new System.Drawing.Size(150, 20);
+            this.txtIPAddress.TabIndex = 2;
+            this.txtIPAddress.Text = "127.0.0.1";
+            // 
+            // btnLeaderboard
+            // 
+            this.btnLeaderboard.Location = new System.Drawing.Point(12, 70);
+            this.btnLeaderboard.Name = "btnLeaderboard";
+            this.btnLeaderboard.Size = new System.Drawing.Size(100, 23);
+            this.btnLeaderboard.TabIndex = 3;
+            this.btnLeaderboard.Text = "Leaderboard";
+            this.btnLeaderboard.UseVisualStyleBackColor = true;
+            this.btnLeaderboard.Click += new System.EventHandler(this.btnLeaderboard_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnLeaderboard);
+            this.Controls.Add(this.txtIPAddress);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnStartGame);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStartGame;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtIPAddress;
+        private System.Windows.Forms.Button btnLeaderboard;
     }
 }
-
