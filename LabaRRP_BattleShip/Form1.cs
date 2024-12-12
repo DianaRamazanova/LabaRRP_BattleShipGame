0a089a992359699aca56536d1dc4524b9896fe14
poniny <blindaglinda@gmail.com>
2024-12-12 14:50:08 +0300

    форма1

--- LabaRRP_BattleShip/Form1.cs
+++ LabaRRP_BattleShip/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using LabaRRP_BattleShip.ClientServer;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,7 @@ namespace LabaRRP_BattleShip
 {
     public partial class Form1 : Form
     {
+        private Client _client;
         private Game _game;
         public Form1()
         {
@@ -19,9 +21,24 @@ namespace LabaRRP_BattleShip
         }
         private void btnStartGame_Click(object sender, EventArgs e)
         {
+            _client = new Client();
             _game = new Game();
-            //SetupForm setupForm = new SetupForm(_game);
-            // setupForm.ShowDialog();
+            SetupForm setupForm = new SetupForm(_game);
+            setupForm.ShowDialog();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            _client.Connect(txtIPAddress.Text);
+            _game = new Game();
+            SetupForm setupForm = new SetupForm(_game);
+            setupForm.ShowDialog();
+        }
+
+        private void btnLeaderboard_Click(object sender, EventArgs e)
+        {
+            LeaderboardForm leaderboardForm = new LeaderboardForm();
+            leaderboardForm.ShowDialog();
         }
     }
 }
